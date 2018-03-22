004dcb914c47868ba3313cc1858a1a57ef81f755
Keith VenHuizen <keith.venhuizen@gmail.com>
2018-03-22 15:30:41 -0500

    Finish microposts without images

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -76,5 +76,14 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  # ===== DEPENDENT DESTROY MICROPOSTS =====
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
   
 end
